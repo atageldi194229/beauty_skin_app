d066a820d83a4c42c2051bcd8e957467e4386afd
rustamcharyyev <cibersport4@gmail.com>
2023-07-13 21:06:14 +0500

    asd

--- lib/data/api/api_client.dart
+++ lib/data/api/api_client.dart
@@ -4,7 +4,9 @@ import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 final localhostOptions = BaseOptions(
   baseUrl: kBaseUrl,
-  // connectTimeout: 5000,
+  connectTimeout: const Duration(milliseconds: 5000),
+  receiveTimeout: const Duration(milliseconds: 5000),
+  sendTimeout: const Duration(milliseconds: 5000),
   // receiveTimeout: 3000,
   // sendTimeout: 3000,
 );
